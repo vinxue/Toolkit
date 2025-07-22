52fa7a81815001fbaf2818c29f4d102f4f21ebde
Vin Xue <vinxue@outlook.com>
2025-07-22 08:30:03 +0800

    Refine code structure

--- TimeTracker/TimeTracker/Properties/AssemblyInfo.cs
+++ TimeTracker/TimeTracker/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TimeTracker")]
-[assembly: AssemblyCopyright("Copyright © 2025 Gavin")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
